ae83e8c0113c4ef05ce533b8b606ff6503862a25
pickles-code <todesign0515@gmail.com>
2022-11-27 11:42:16 +0900

    Station6クリア

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@
 
 import * as React from 'react'
 import './App.css'
+import { useState } from 'react';
+
+
 
 /**
  * 
  * @type {React.FC}
  */
+
+
+
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState([])
+
+  const handleClick = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDogUrl(result);
+          console.log(result);
+        }
+      )
+  };
+
   return (
     <div>
       <header>
         <h1>React-Stations</h1>
       </header>
       <p>description</p>
-      <img src="https://images.dog.ceo/breeds/terrier-fox/n02095314_1582.jpg" alt="" />
+      <img src={dogUrl.message} alt="" />
+      <button onClick={handleClick}>change</button>
     </div>
   )
 }
